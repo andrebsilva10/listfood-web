72afc83b0ae0a6f41b1ebb6fcac140433164f49e
André <andrebsilva75@gmail.com>
2024-05-14 22:35:37 -0300

    login page, interpolation and Event Binding to display messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,17 @@ module.exports = {
   plugins: [
     require('daisyui')
   ],
+    daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#00B7C6",
+          secondary: "#0C5274",
+          "base-200": "#f0f0f0",
+        },
+      },
+      "dim",
+    ],
+  },
 }
