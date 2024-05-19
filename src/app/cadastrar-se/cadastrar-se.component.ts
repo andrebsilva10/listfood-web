ad4bc24d0de168f4a03a9ca2546283ed5f52a20a
André <andrebsilva75@gmail.com>
2024-05-18 23:40:52 -0300

    Configured routes and added "Cadastrar-se" component

---
+++ src/app/cadastrar-se/cadastrar-se.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-se',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastrar-se.component.html',
+  styleUrl: './cadastrar-se.component.scss'
+})
+export class CadastrarSeComponent {
+
+}
